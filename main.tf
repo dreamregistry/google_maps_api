b8ab438799f8d00c76f4dd767af50afd8a7ebb59
Jonatan Sombié <jonatan.sombie@gmail.com>
2024-04-19 08:57:10 +0200

    Add Google Maps API server key as resource
    The commit includes a new variable for the Google Maps API server key in the Terraform configuration. The server key is added as an AWS SSM parameter. Additionally, the output for this key has been defined to provide information such as Arn, key, current region and parameter type.

--- main.tf
+++ main.tf
@@ -22,6 +22,12 @@ variable "google_maps_api_key" {
   type        = string
 }
 
+variable "google_maps_api_server_key" {
+  description = "google maps api server key"
+  type        = string
+  default     = null
+}
+
 resource "random_pet" "google_maps_api" {
   length = 2
 }
@@ -33,14 +39,36 @@ resource "aws_ssm_parameter" "google_maps_api_key" {
   value       = var.google_maps_api_key
 }
 
+resource "aws_ssm_parameter" "google_maps_api_server_key" {
+  count = var.google_maps_api_server_key != null ? 1 : 0
+  name        = "/google_maps_api/${random_pet.google_maps_api.id}/server_key"
+  description = "google maps api server key"
+  type        = "SecureString"
+  value       = var.google_maps_api_server_key
+}
+
 data "aws_region" "current" {}
 
 output "GOOGLE_MAPS_API_KEY" {
   value = {
-    arn: aws_ssm_parameter.google_maps_api_key.arn
-    key: aws_ssm_parameter.google_maps_api_key.name
-    region: data.aws_region.current.name
-    type: "ssm"
+    arn : aws_ssm_parameter.google_maps_api_key.arn
+    key : aws_ssm_parameter.google_maps_api_key.name
+    region : data.aws_region.current.name
+    type : "ssm"
+  }
+}
+
+output "GOOGLE_MAPS_API_SERVER_KEY" {
+  value = var.google_maps_api_server_key != null ? {
+    arn : aws_ssm_parameter.google_maps_api_server_key[0].arn
+    key : aws_ssm_parameter.google_maps_api_server_key[0].name
+    region : data.aws_region.current.name
+    type : "ssm"
+  } : {
+    arn : aws_ssm_parameter.google_maps_api_key.arn
+    key : aws_ssm_parameter.google_maps_api_key.name
+    region : data.aws_region.current.name
+    type : "ssm"
   }
 }
 
